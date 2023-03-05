7d76fbc7c510baa071a1a86d615970681b1f03d2
linhuibin <1032352370@qq.com>
2023-03-05 11:27:40 +0800

    feat: session 会话

---
+++ 06-session/src/controllers/login.js
@@ -0,0 +1,19 @@
+const { Router } = require('express');
+
+class LoginController {
+    async init() {
+        const router = Router();
+        router.post('/', this.post);
+        return router;
+    }
+
+    post = (req, res) => {
+        req.session.logined = true;
+        res.redirect('/');
+    };
+}
+
+module.exports = async () => {
+    const c = new LoginController();
+    return await c.init();
+};
---
+++ 06-session/src/middlewares/index.js
@@ -0,0 +1,17 @@
+const { Router } = require('express');
+const cookieParser = require('cookie-parser');
+
+const sessionMiddleware = require('./session');
+const urlnormalizeMiddleware = require('./urlNormalize');
+const loginMiddleware = require('./login');
+
+const secret = '842d918ced1888c65a650f993077c3e36c8f114d';
+
+module.exports = async function initMiddlewares() {
+    const router = Router();
+    router.use(urlnormalizeMiddleware());
+    router.use(cookieParser(secret));
+    router.use(sessionMiddleware(secret));
+    router.use(loginMiddleware());
+    return router;
+};
